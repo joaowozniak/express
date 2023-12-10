00de9eb2a7a3f47c8d77d75adb1d06af38d98961
Joao <joao.wozniak@gmail.com>
2023-12-10 20:10:23 +0000

    menu

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for serving the HTML file
 app.get('/', (req, res) => {
-    res.send('Hello, TypeScript Express Test!');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
